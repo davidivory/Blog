e63609ca2814666655acac65b00203cb879addb8
David Ivory <davidmivory@gmail.com>
2017-05-22 19:28:29 -0500

    Add files via upload
    New thoughts appear at the top.

--- blog.js
+++ blog.js
@@ -1,52 +1,50 @@
-function get_todos() {
-	var thoughts = new Array;
-	var thoughts_str = localStorage.getItem('todo');
-	if (thoughts_str !== null) {
-		thoughts = JSON.parse(thoughts_str); 
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
 }
-	return thoughts;
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
 }
-  
-function add() {
-	var task = document.getElementById('task').value;
-  
-	var todos = get_todos();
-	todos.push(task);
-	localStorage.setItem('todo', JSON.stringify(todos));
-  
-	show();
-  
-	return false;
- }
-  
- function remove() {
-	var id = this.getAttribute('id');
-	var todos = get_todos();
-	todos.splice(id, 1);
-	localStorage.setItem('todo', JSON.stringify(todos));
-  
-	show();
-  
-	return false;
- }
-  
-function show() {
-	var todos = get_todos();
-  
-	var html = '<ul>';
-	for(var i=0; i<todos.length; i++) {
-		html += '<li>' + todos[i] + '<button class="remove" id="' + i  + '">x</button></li>';
-	};
-	html += '</ul>';
-  
-	document.getElementById('todos').innerHTML = html;
-  
-	var buttons = document.getElementsByClassName('remove');
-	for (var i=0; i < buttons.length; i++) {
-		buttons[i].addEventListener('click', remove);
-	};
+
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
 }
-  
- document.getElementById('add').addEventListener('click', add);
- show();
+
 
